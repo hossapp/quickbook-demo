19ae2683146724100288e3ecb5eab31083002940
Trung Vu <trung@apitracker.com>
2020-09-10 09:34:19 -0500

    get json transform & flow to work with new runtime

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
-const execute = require('@hossapp/runtime').default
+const Runtime = require('@hossapp/runtime').default
 
 if (process.argv.length < 3) {
   throw new Error("Need manifest file")
 }
 
 const manifest = require(process.argv[2])
-execute(manifest)
+const runtime = new Runtime(manifest)
+runtime.start()
